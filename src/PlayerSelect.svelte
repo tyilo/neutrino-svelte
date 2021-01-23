448657cedf2566bb37e3b3c58aa244d084ba8952
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2021-01-23 19:45:36 +0100

    Fix svelte-check error

--- src/PlayerSelect.svelte
+++ src/PlayerSelect.svelte
@@ -1,9 +1,30 @@
 <script lang="ts">
   export let isHuman = true;
+
+  // We can't bind a boolean to the inputs' value
+  $: _isHuman = Number(isHuman);
+
+  function handleChange() {
+    isHuman = Boolean(_isHuman);
+  }
 </script>
 
-<label> <input type="radio" bind:group={isHuman} value={true} /> Human </label>
-<label> <input type="radio" bind:group={isHuman} value={false} /> Bot </label>
+<label>
+  <input
+    type="radio"
+    bind:group={_isHuman}
+    value={1}
+    on:change={handleChange}
+  /> Human
+</label>
+<label>
+  <input
+    type="radio"
+    bind:group={_isHuman}
+    value={0}
+    on:change={handleChange}
+  /> Bot
+</label>
 
 <style>
   label {
